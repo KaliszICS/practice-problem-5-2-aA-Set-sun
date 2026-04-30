dc257cd4d6b47c69047d91cf6225e887b338d023
A.Set <343303426@tdsb.ca>
2026-04-30 10:59:42 -0400

    um

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -2,7 +2,7 @@
 title: Lesson 5.2 - Arrays, Searching, Copying, Deleting
 name: Angie Seto
 date created: April 29, 2026
-date updated: April 29, 2026
+date updated: April 30, 2026
 */
 
 public class PracticeProblem {
@@ -28,18 +28,34 @@ public class PracticeProblem {
 	//q2
 	public static int findThirdValue(String word, String[] arr) {
 		int result = -1;
-		
+		int num = 0;
 		for (int i = 0; i < arr.length; i++) {
-			if (arr[i].matches(word)) {
+			if (arr[i].equals(word)) {
+				num++;
+				if (num == 3) {
 				result = i;
-				i = arr.length; 
+				i = arr.length;
+				} 
 			}
 		}
 		return result; 
 	}
 
 	// //q3
-	// public static double[] append (double num1, double[] arr) {
+	public static double[] append (double num1 /*new element*/, double[] arr) {
+		double[] arr2 = new double[arr.length + 1];
+		int endSpot = arr2.length - 1;
+		int ogIndex = 0; 
 
-	// }
+		for (int i = 0; i < arr2.length; i++) {
+			if (i == endSpot) {
+				arr2[i] = num1;
+			}
+			else {
+				arr2[i] = arr[ogIndex];
+				ogIndex++;
+			}
+		}
+		return arr2;
+	}
 }
